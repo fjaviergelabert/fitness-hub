3c138b8b433bff44ee4413400dd548d821478cb7
Kiko <fjavier.gelabert@gmail.com>
2024-01-26 12:30:27 -0800

    Create Exercises page

--- app/exercises/page.tsx
+++ app/exercises/page.tsx
@@ -1,74 +1,32 @@
 "use client";
-import { exerciseSchema } from "@/schemas/exercise";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { Exercise } from "@prisma/client";
-import { Button, Heading, Text, TextArea, TextField } from "@radix-ui/themes";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import axios, { AxiosError } from "axios";
-import { FieldValues, useForm } from "react-hook-form";
-import { AiOutlineLoading } from "react-icons/ai";
-import { toast } from "react-toastify";
+import { Table } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 function Exercises() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isValid },
-  } = useForm<Exercise>({ resolver: zodResolver(exerciseSchema) });
-  const queryClient = useQueryClient();
-  const exercise = useMutation({
-    mutationFn: (exercise: Exercise) =>
-      axios.post("/api/exercises", exercise).then((res) => res.data),
-    onError: async (e: AxiosError<{ error: string }>) => {
-      if ((e.status = 400)) {
-        toast.warning(e.response?.data.error, {
-          theme: "colored",
-        });
-      } else {
-        toast.error("An error ocurred.", {
-          theme: "colored",
-        });
-      }
-    },
-    onSuccess: async () => {
-      toast.success("TRRRRRansaction completed", { theme: "colored" });
-      queryClient.invalidateQueries({ queryKey: ["exercises"] });
-    },
+  const exercises = useQuery<Exercise[]>({
+    queryKey: ["exercises"],
+    queryFn: () => axios.get("/api/exercises").then((res) => res.data),
   });
-
-  async function createExercise(formValues: FieldValues) {
-    exercise.mutate(formValues as Exercise);
-  }
-
   return (
-    <>
-      <Heading as="h1">Create Exercise</Heading>
-      <form
-        onSubmit={handleSubmit(createExercise)}
-        className="flex flex-col gap-3 max-w-screen-lg"
-      >
-        <fieldset className="max-w-sm">
-          <Text as="label">
-            Exercise Name
-            <TextField.Input {...register("name")} />
-          </Text>
-          {errors.name && <Text color="crimson">{errors.name?.message}</Text>}
-        </fieldset>
-        <fieldset>
-          <Text as="label">
-            Description
-            <TextArea {...register("description")} />
-          </Text>
-          {errors.description && (
-            <Text color="crimson">{errors.description?.message}</Text>
-          )}
-        </fieldset>
-        <Button type="submit" disabled={exercise.isPending || !isValid}>
-          CREATE
-          {exercise.isPending && <AiOutlineLoading className="animate-spin" />}
-        </Button>
-      </form>
-    </>
+    <Table.Root>
+      <Table.Header>
+        <Table.Row>
+          <Table.ColumnHeaderCell>Name</Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell>Description</Table.ColumnHeaderCell>
+        </Table.Row>
+      </Table.Header>
+
+      <Table.Body>
+        {exercises.data?.map((exercise) => (
+          <Table.Row key={exercise.id}>
+            <Table.RowHeaderCell>{exercise.name}</Table.RowHeaderCell>
+            <Table.Cell>{exercise.description}</Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table.Root>
   );
 }
 
