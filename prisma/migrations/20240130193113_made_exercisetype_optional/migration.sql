b07755543e6a36212175022b93c93eb7084f6473
Kiko <fjavier.gelabert@gmail.com>
2024-01-31 09:44:28 -0800

    Create Workout POST api

---
+++ prisma/migrations/20240130193113_made_exercisetype_optional/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `blockexercise` MODIFY `type` ENUM('TIMER', 'COUNTDOWN', 'MAX_REPS') NULL;
--- schemas/exercise.ts
+++ schemas/exercise.ts
@@ -1,3 +1,4 @@
+import { ExerciseType } from "@prisma/client";
 import { z } from "zod";
 
 export const exerciseSchema = z.object({
@@ -5,3 +6,19 @@ export const exerciseSchema = z.object({
   description: z.string().max(255).optional().nullable(),
   mediaUrl: z.string().max(255).optional().nullable(),
 });
+
+export const workoutExerciseSchema = exerciseSchema.extend({
+  id: z.number().optional().nullable(),
+  type: z.nativeEnum(ExerciseType).optional(),
+});
+
+export const workoutSchema = z.object({
+  name: z.string().min(3).max(150),
+  description: z.string().max(255).optional().nullable(),
+  exercises: z.array(workoutExerciseSchema).nonempty({
+    message: "Exercises need to be added to the workout.",
+  }),
+});
+
+export type Workout = z.infer<typeof workoutSchema>;
+export type WorkoutExercise = z.infer<typeof workoutExerciseSchema>;
