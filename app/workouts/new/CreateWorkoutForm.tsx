9e71e9e161a9c2a388cefef69bab7947c82305a4
Kiko <fjavier.gelabert@gmail.com>
2024-02-03 19:53:58 -0800

    Code refactor: Move Exercise section to its own component

--- app/workouts/new/CreateWorkoutForm.tsx
+++ app/workouts/new/CreateWorkoutForm.tsx
@@ -6,21 +6,14 @@ import { Exercise } from "@prisma/client";
 import { Button, Heading, Text, TextArea, TextField } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-import { useForm } from "react-hook-form";
+import { UseFormReturn, useForm } from "react-hook-form";
 import { ExerciseCard } from "./ExerciseCard";
 import { ExerciseMenu } from "./ExerciseMenu";
 import { RemoveExerciseButton } from "./RemoveExerciseButton";
 
 export function CreateWorkoutForm(props: { exercises: Exercise[] }) {
   const router = useRouter();
-
-  const {
-    register,
-    handleSubmit,
-    setValue,
-    getValues,
-    formState: { errors, isValid },
-  } = useForm<Workout>({
+  const form = useForm<Workout>({
     resolver: zodResolver(workoutSchema),
     defaultValues: {
       name: "",
@@ -28,6 +21,14 @@ export function CreateWorkoutForm(props: { exercises: Exercise[] }) {
       exercises: [],
     },
   });
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    getValues,
+    formState: { errors, isValid },
+  } = form;
+
   const formExercises = getValues("exercises");
 
   function createWorkout(workout: Workout) {
@@ -73,35 +74,49 @@ export function CreateWorkoutForm(props: { exercises: Exercise[] }) {
         )}
       </fieldset>
 
-      <fieldset className="flex flex-col gap-3">
-        <Heading as="h3">Exercises: </Heading>
-        {formExercises.map((exercise) => (
-          <ExerciseCard
-            key={exercise.id}
-            exercise={exercise}
-            onSelect={(type) => {
-              const newValue: any = formExercises.map((e) =>
-                e.id === exercise.id ? { ...e, type } : e
-              );
-              setValue("exercises", newValue);
-            }}
-            buttonsSection={
-              <RemoveExerciseButton
-                onClick={() => {
-                  const newValue: any = formExercises.filter((e) =>
-                    e.id ? e.id !== exercise.id : e.name !== exercise.name
-                  );
-                  setValue("exercises", newValue);
-                }}
-              />
-            }
-          />
-        ))}
-        {errors.exercises && (
-          <Text color="crimson">{errors.exercises?.message}</Text>
-        )}
-      </fieldset>
+      <WorkoutExercises form={form} />
+
       <Button type="submit">Save</Button>
     </form>
+  );
+}
+
+function WorkoutExercises({ form }: { form: UseFormReturn<Workout> }) {
+  const {
+    getValues,
+    setValue,
+    formState: { errors },
+  } = form;
+  const exercises = getValues("exercises");
+
+  return (
+    <fieldset className="flex flex-col gap-3">
+      <Heading as="h3">Exercises: </Heading>
+      {exercises.map((exercise) => (
+        <ExerciseCard
+          key={exercise.id}
+          exercise={exercise}
+          onSelect={(type) => {
+            const newValue: any = exercises.map((e) =>
+              e.id === exercise.id ? { ...e, type } : e
+            );
+            setValue("exercises", newValue);
+          }}
+          buttonsSection={
+            <RemoveExerciseButton
+              onClick={() => {
+                const newValue: any = exercises.filter((e) =>
+                  e.id ? e.id !== exercise.id : e.name !== exercise.name
+                );
+                form.setValue("exercises", newValue);
+              }}
+            />
+          }
+        />
+      ))}
+      {errors.exercises && (
+        <Text color="crimson">{errors.exercises?.message}</Text>
+      )}
+    </fieldset>
   );
 }
