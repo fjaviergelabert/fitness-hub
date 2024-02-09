8fa37b1f74db2d7d5161dc9a07fc31f60784abdf
Kiko <fjavier.gelabert@gmail.com>
2024-02-08 17:11:40 -0800

    Made Remove button use pointer as cursor

--- app/workouts/_components/RemoveButton.tsx
+++ app/workouts/_components/RemoveButton.tsx
@@ -2,15 +2,18 @@
 import { Button } from "@radix-ui/themes";
 import { ImCross } from "react-icons/im";
 
-export function RemoveButton({ onClick }: { onClick: () => any }) {
+export function RemoveButton({ onClick }: any & { onClick: () => any }) {
   return (
     <Button
       onClick={(e) => {
         e.preventDefault();
         onClick();
       }}
+      asChild
     >
-      <ImCross size={"1rem"} />
+      <span className="cursor-pointer">
+        <ImCross size={"1rem"} />
+      </span>
     </Button>
   );
 }
