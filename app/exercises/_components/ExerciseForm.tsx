d8ae5ae7b7ea4ef32074973a72baa6cd8ed6be6d
Kiko <fjavier.gelabert@gmail.com>
2024-04-10 11:17:56 -0700

    Refactor ExerciseForm: Applied principle of Inversion of Control, in order to be reusable and customized from a future Dialog Component

--- app/exercises/_components/ExerciseForm.tsx
+++ app/exercises/_components/ExerciseForm.tsx
@@ -4,47 +4,77 @@ import { exerciseSchema } from "@/schemas/exercise";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Exercise } from "@prisma/client";
 import { Button, Callout, Text, TextArea, TextField } from "@radix-ui/themes";
-import { MutationFunction } from "@tanstack/react-query";
 import axios from "axios";
-import { useForm } from "react-hook-form";
+import { ReactNode } from "react";
+import { FormProvider, useForm } from "react-hook-form";
 import { AiOutlineLoading } from "react-icons/ai";
 import { CiCircleInfo } from "react-icons/ci";
 
 export function EditForm(props: { exercise: Exercise }) {
+  const exerciseMutation = useMutation((exercise: Exercise) => {
+    return axios
+      .put("/api/exercises/" + props.exercise.id, exercise)
+      .then((res) => res.data);
+  }, "/exercises");
+
   return (
     <ExerciseForm
       onSubmit={(exercise: Exercise) => {
-        return axios
-          .put("/api/exercises/" + props.exercise.id, exercise)
-          .then((res) => res.data);
+        exerciseMutation.mutate(exercise);
       }}
       {...props}
+      buttonSection={
+        <Button
+          className="self-end"
+          type="submit"
+          disabled={exerciseMutation.isPending}
+        >
+          SAVE
+          {exerciseMutation.isPending && (
+            <AiOutlineLoading className="animate-spin" />
+          )}
+        </Button>
+      }
     />
   );
 }
 
 export function CreateForm() {
+  const exerciseMutation = useMutation((exercise: Exercise) => {
+    return axios.post("/api/exercises", exercise).then((res) => res.data);
+  }, "/exercises");
+
   return (
     <ExerciseForm
       onSubmit={(exercise: Exercise) => {
-        return axios.post("/api/exercises", exercise).then((res) => res.data);
+        exerciseMutation.mutate(exercise);
       }}
+      buttonSection={
+        <Button
+          className="self-end"
+          type="submit"
+          disabled={exerciseMutation.isPending}
+        >
+          SAVE
+          {exerciseMutation.isPending && (
+            <AiOutlineLoading className="animate-spin" />
+          )}
+        </Button>
+      }
     />
   );
 }
 
 export function ExerciseForm({
   exercise,
+  buttonSection,
   onSubmit,
 }: {
   exercise?: Exercise;
-  onSubmit: MutationFunction<Exercise, Exercise>;
+  buttonSection: ReactNode;
+  onSubmit: (e: Exercise) => void;
 }) {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isValid, isSubmitted },
-  } = useForm<Exercise>({
+  const form = useForm<Exercise>({
     resolver: zodResolver(exerciseSchema),
     defaultValues: exercise || {
       id: 0,
@@ -53,50 +83,44 @@ export function ExerciseForm({
       mediaUrl: "",
     },
   });
-  const exerciseMutation = useMutation(onSubmit, "/exercises");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid, isSubmitted },
+  } = form;
+
   return (
-    <form
-      onSubmit={handleSubmit((formValues) => {
-        if (isValid) {
-          exerciseMutation.mutate(formValues);
-        }
-      })}
-      className="flex flex-col gap-3 max-w-screen-lg"
-    >
-      {isSubmitted && !isValid && (
-        <Callout.Root color="crimson">
-          <Callout.Icon>
-            <CiCircleInfo />
-          </Callout.Icon>
-          <Callout.Text>The form is not valid.</Callout.Text>
-        </Callout.Root>
-      )}
-      <fieldset className="max-w-sm">
-        <Text as="label">
-          Exercise Name
-          <TextField.Input {...register("name")} />
-        </Text>
-        {errors.name && <Text color="crimson">{errors.name?.message}</Text>}
-      </fieldset>
-      <fieldset>
-        <Text as="label">
-          Description
-          <TextArea {...register("description")} />
-        </Text>
-        {errors.description && (
-          <Text color="crimson">{errors.description?.message}</Text>
-        )}
-      </fieldset>
-      <Button
-        className="self-end"
-        type="submit"
-        disabled={exerciseMutation.isPending}
+    <FormProvider {...form}>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-3 max-w-screen-lg"
       >
-        SAVE
-        {exerciseMutation.isPending && (
-          <AiOutlineLoading className="animate-spin" />
+        {isSubmitted && !isValid && (
+          <Callout.Root color="crimson">
+            <Callout.Icon>
+              <CiCircleInfo />
+            </Callout.Icon>
+            <Callout.Text>The form is not valid.</Callout.Text>
+          </Callout.Root>
         )}
-      </Button>
-    </form>
+        <fieldset className="max-w-sm">
+          <Text as="label">
+            Exercise Name
+            <TextField.Input {...register("name")} />
+          </Text>
+          {errors.name && <Text color="crimson">{errors.name?.message}</Text>}
+        </fieldset>
+        <fieldset>
+          <Text as="label">
+            Description
+            <TextArea {...register("description")} />
+          </Text>
+          {errors.description && (
+            <Text color="crimson">{errors.description?.message}</Text>
+          )}
+        </fieldset>
+        {buttonSection}
+      </form>
+    </FormProvider>
   );
 }
