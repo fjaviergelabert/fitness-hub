2ce55e44bce00a401b11cb0265b5852f1164ba4c
Kiko <fjavier.gelabert@gmail.com>
2024-07-03 17:46:47 -0700

    Migrate useless APIs after migration to Server Actions

--- app/api/workouts/route.ts
+++
@@ -1,9 +0,0 @@
-import prisma from "@/prisma/client";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const workouts = await prisma.block.findMany({
-    include: { exercises: true },
-  });
-  return NextResponse.json(workouts);
-}
